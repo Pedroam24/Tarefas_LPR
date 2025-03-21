fcd940ec10c7fa4cae41d90ce90c30b0cb7e8dfd
Pedroam24 <pedroalmartins09@gmail.com>
2025-03-21 08:42:31 -0300

    impar_ou_par

---
+++ par_impar.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("DESCOBRINDO SE O NÚMERO É PAR!!!");
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite um número: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // se a divisão por dois der zero, o numero e par
+        if (number % 2 == 0){
+            Console.Write(number + " é par");
+        }
+        else{
+            Console.Write(number + " é ímpar");
+        }
+
+    }
+}
+

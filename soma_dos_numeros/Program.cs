59f6b947694329f89386a68fc64aba8e831cc799
Pedroam24 <pedroalmartins09@gmail.com>
2025-03-28 15:31:55 -0300

    Tarefas

---
+++ soma_dos_numeros/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+class program
+{
+   static void Main()
+   {
+    int soma = 0, num, result;
+    Console.Write("Digite um número:");
+    num = int.Parse(Console.ReadLine());
+    result = num * num;
+    Console.WriteLine("o número ao quadrado é:" + result);
+    while (result > 0)
+    {
+    soma+=result % 10;
+    result/=10;
+    }
+    Console.WriteLine("A soma de cada algaritimo do seu numero elevado ao quadrado é: "+ soma);
+  }
+}

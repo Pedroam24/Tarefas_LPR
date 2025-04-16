53bf6c47d86961e8f2f54c99854b417706f57c49
Pedroam24 <pedroalmartins09@gmail.com>
2025-04-16 15:27:03 -0300

    tarefas_lpr

--- aula_6/exercicio1.cpp
+++ aula_6/exercicio1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
-
+int retornoinverso(int num){
+   int reverso = 0 ;
+while (num != 0){
+    int digito;
+    digito = num % 10 ;
+    reverso = reverso * 10 + digito; 
+    num /= 10;
+}
+return reverso;
+}
 int main(){
-    cout << "hello world" ;
-    return 0;
+    int x ;
+    cout << "Digite um numero inteiro: ";
+    cin >> x;
+
+    int result = retornoinverso(x);
+    cout << result <<"hello world" ;
+    
 }

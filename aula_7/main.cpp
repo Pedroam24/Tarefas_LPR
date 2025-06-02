6d557737e899dc70c7ef250819d7daafd6017222
Pedroam24 <pedroalmartins09@gmail.com>
2025-06-02 11:39:08 -0300

    tarefas

--- aula_7/main.cpp
+++ aula_7/main.cpp
@@ -3,27 +3,42 @@
 using namespace std;
 
 int main(){
-  int num;  
-  int par[5];
-  int imp[4];
-  int cpar, cimp;
+  int numeros[10];  
+  int num;
+  int par[10];
+  int imp[10];
+  int i = 0;
+  cout << "Digite numeros inteiros" << endl;
+  for ( i = 0; i < 10; i++){
+    
+    cout << "numero" << i << ": " << endl;
+    cin >> num;
+    numeros[i] = num;
+    
+  }
+  i = 0;
+  for(int num: numeros)
+  {
+    if(num%2==0){
+    //cout << "Numeros pares: " << numeros[i] << endl; 
+        par[i] = numeros[i];
 
-  cout << "Digite numeros entre 1 e 10" << endl;
-    for (int i = 0; i < 10; i++) {
-      cout << "numero"<< i << ": ";
-      cin >> num;
-    
-      if (num%2==0){
-        par[5] = num; 
-        i++;
-      }
-      else{
-        imp[4] = num;
-        i++;
-      }
-    
+    i++;
+
+
     }
+    else {
+    //cout << "Numeros impares: " << numeros[i] << endl; 
+    imp[i] = numeros[i];
+    i++;
+    
 
-    cout <<  "pares: " << par[1] << ", " << par[2] << ", " <<  par[3] << ", " << par[4] << ", " << "," << par[5] << endl;
-  
+    }
+  } 
+  cout << "numeros pares: " << par[i] << endl;
+  cout << "numeros impares: " << imp[i] << endl;
+
+
+
+
   }

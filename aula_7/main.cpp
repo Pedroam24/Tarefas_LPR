8af9a47f3cf7bd91bfbca6396843458e47331903
Pedroam24 <pedroalmartins09@gmail.com>
2025-06-13 09:39:21 -0300

    tarefas

--- aula_7/main.cpp
+++ aula_7/main.cpp
@@ -7,36 +7,32 @@ int main(){
   int num;
   int par[10];
   int imp[10];
+  int contpar = 0;
+  int contimp = 0;
   int i = 0;
   cout << "Digite numeros inteiros" << endl;
   for ( i = 0; i < 10; i++){
     
-    cout << "numero" << i << ": " << endl;
+    cout << "numero" << i + 1 << ": " << endl;
     cin >> num;
     numeros[i] = num;
-    
+     if (num%2==0){
+      contpar++;
+      par[contpar] = numeros[i];
+     }
+    else{
+      contimp++;
+      imp[contimp] = numeros[i];
+    }
   }
-  i = 0;
-  for(int num: numeros)
-  {
-    if(num%2==0){
-    //cout << "Numeros pares: " << numeros[i] << endl; 
-        par[i] = numeros[i];
 
-    i++;
-
-
-    }
-    else {
-    //cout << "Numeros impares: " << numeros[i] << endl; 
-    imp[i] = numeros[i];
-    i++;
-    
-
-    }
-  } 
+  for ( int i = 0; i<contpar ; i++){
   cout << "numeros pares: " << par[i] << endl;
+}
+
+  for ( int i = 0; i<contimp ; i++){
   cout << "numeros impares: " << imp[i] << endl;
+}
 
 
 

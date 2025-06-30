515bd0ac966d7da07447b4505bf2690d79701886
Pedroam24 <pedroalmartins09@gmail.com>
2025-06-30 11:39:52 -0300

    tarefas

---
+++ aula_9/exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class program
+{
+    static void Main()
+    {
+        List<string> nomes = new();
+
+        Console.WriteLine("digite a quantidade de nomes da sua lista: ");
+        int quant = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= quant; i++)
+        {
+            Console.WriteLine("Digite o " + i + 1 + "° nome ");
+            nomes.Add(Console.ReadLine()); 
+            
+
+        }
+            
+
+
+    }
+}
--- exemplo/Program.cs
+++ exemplo/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections.Generic;
-class program {
+class program
+{
     static void Main()
     {
         List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
-        //Console.WriteLine(numeros[2]);
         numeros.Add(4);
         Console.Write(numeros[5]);
         numeros.Insert(1, 7);
@@ -35,10 +35,24 @@ class program {
 
         }
         numeros.RemoveAt(2);
-        Console.WriteLine(numeros[2]);
+        Console.WriteLine("número que ficou no lugar 2: " + numeros[2]);
+
+        foreach (int num in numeros)
+        {
+            numeros.Remove(4);
+            Console.WriteLine("números presente na lista" + numeros[1] + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6]);
+
+            
+        }
     }
+   
+ 
+        
+    
+
+    
  }
  
-//7. Remover o número 2
+
 //8. Remover todos os números 4
 

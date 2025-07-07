4663f5203d64f7984207fbe7a7999616c1347814
Pedro <pedroalmartins09@gmail.com>
2025-07-07 11:35:04 -0300

    Add files via upload

---
+++ aula_9/exercicios_de_dicionario/dicionario/exercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace exercicio1;
+    using System;
+    using System.Collections.Generic;   
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> alunos = new Dictionary<string, int> { 
+            { "Pedro", 17 }, {"Gigi", 18 }, {"luis", 20}, {"Maria", 18}  
+            };
+
+            double media = alunos.Values.Sum() / (double)alunos.Count;
+           Console.WriteLine("A média de idade dos alunos é: " + media);
+        foreach (var students in alunos.Values)
+        {
+            if (students > media)
+            {
+                Console.WriteLine(alunos.Key + " é maior que a média de ideade de alunos.");
+            }
+            else
+            {
+                Console.WriteLine(alunos.Key + " é menor que a média de idade dos alunos.");
+            
+        }
+        }//fim do main
+    }
+
